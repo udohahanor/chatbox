3c54225d6cefaf7657dd68d04374d80b4e89fa6a
udohahanor <udohahanor@gmail.com>
2022-04-04 11:18:32 +0100

    chat window

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,8 +5,61 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.blue,
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            const SizedBox(height: 20.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Row(
+                children: [
+                  const Text(
+                    'Chatbox',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.3,
+                    ),
+                  ),
+                  const Spacer(),
+                  Row(
+                    children: const [
+                      Icon(
+                        Icons.add,
+                        size: 20.0,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: 20.0),
+                      Icon(
+                        Icons.search_outlined,
+                        size: 20.0,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20.0),
+            Container(
+              width: 50.0,
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
